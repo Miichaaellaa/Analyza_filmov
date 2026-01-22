efc51918e7ce6a50ef4f6e2440452a3a42062c11
Juraj <kvasnakjuraj2112@gmail.com>
2026-01-22 22:57:54 +0100

    Modelovanie Movie v1

---
+++ src/sk/spse/movie/Movie.java
@@ -0,0 +1,42 @@
+package sk.spse.movie;
+
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private int year;
+    private List<String> cast;
+    private List<String> genres;
+
+    public Movie() {
+    }
+
+    public Movie(String title, int year, List<String> cast, List<String> genres) {
+        this.title = title;
+        this.year = year;
+        this.cast = cast;
+        this.genres = genres;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getCast() {
+        return cast;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    @Override
+    public String toString() {
+        String castInfo = (cast != null && !cast.isEmpty()) ? String.join(", ", cast) : "neznáme obsadenie";
+        return String.format("Film: %s [%d] | Hrajú: %s", title, year, castInfo);
+    }
+}
